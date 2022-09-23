dd6c43e619f46859688539dfd817208ac2356a4f
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-09-23 12:07:37 +0100

    Storing ata in component state via an ajax call. Then rendered api information (state data) using map.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 01. Firstly this component & getUsers method allows us to store date in a component state via an ajax call.
+class App extends Component { 
+  constructor(props) {
+    super(props)
+    // state
+    this.state = {
+      users: []
+    }
+  }
+
+// The getUsers method can be called in the componentDidMount method
+
+  getUsers() {
+    axios('https://randomuser.me/api/?nat=US&results=5')
+    .then(response => this.setState({
+      users: response.data.results
+    })
+    );
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+
+// API data is then rendered below. In this example weve created a unique key for each user by assisgning the user.id to each user.
+// We then render the user's first name in the h3 tag.
+// We then render the user's email in a p tag below.
+// Finally, we created a line seperating each users information using the <hr /> tag.
+
+  render() {
+    return (
+      <div className="App">{this.state.users.map(user => 
+        <div key={user.id}>
+          <h3>{user.name.first}</h3>
+          <p>{user.email}</p>
+          <hr />
+        </div>
+      )}</div>
+    );
+  }
 }
-
 export default App;
